a7d8b044903b3a09568f28b43739821557e2466a
wzx-ipads <593074943@qq.com>
2020-10-19 01:24:40 +0800

    add a ci template

---
+++ tests/example-integration-test.rs
@@ -0,0 +1,12 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn sucess_test_example() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn failure_test_example() {
+        panic!("Make this test fail");
+    }
+}
