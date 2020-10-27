b0e2ded3979f9d779abc609f673fe68bc3018e95
wzx-ipads <593074943@qq.com>
2020-10-27 21:12:55 +0800

    add bootloader

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
-#![no_std] // 不链接Rust标准库
-#![no_main] // 禁用所有Rust层级的入口点
+#![no_std]
+#![no_main]
 
 use core::panic::PanicInfo;
 
-/// 这个函数将在panic时被调用
+static HELLO: &[u8] = b"haha haha!";
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xc;
+        }
+    }
+
+    loop {}
+}
+
+/// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
-
-#[no_mangle] // 不重整函数名
-pub extern "C" fn _start() -> ! {
-    // 因为编译器会寻找一个名为`_start`的函数，所以这个函数就是入口点
-    // 默认命名为`_start`
-    loop {}
-}
-
